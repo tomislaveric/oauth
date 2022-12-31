6e5f3297da828d2aed8993394af41ba298f3dc75
tomislav <hi@tomislav.mobi>
2022-12-31 23:32:03 +0100

    decouple storage from oauth

--- Sources/OAuth/Models/Token.swift
+++ Sources/OAuth/Models/Token.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-public struct Token: Decodable {
+public struct Token: Codable {
     public let token_type: String
     public let refresh_token: String
     public let access_token: String
